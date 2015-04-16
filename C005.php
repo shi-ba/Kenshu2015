158f333bca82c0f45202975dab536e0839b75bae
unknown <yamaguchi@37-50019.Gim.local>
2015-04-16 14:31:42 +0900

    C005修正しました

--- C005.php
+++ C005.php
@@ -1,21 +1,20 @@
 <?php
 //C005問題 全パス
+//修正分をGithub上に反映し忘れていました。申し訳ありませんでした。
 //柴田和樹、中野大輝
 $sou = fgets(STDIN);
 for($i=0;$i<$sou;$i++){
         $ip[$i] = fgets(STDIN);
 }
-
 for($i=0;$i<$sou;$i++){
         $ok = 0;
         $suti = explode(".",$ip[$i]); //explode by dot
         if(count($suti) == 4){
                 for($j=0;$j<4;$j++){
-                        echo "aaa";
-                        if($suti[$j] != null){
-                                if($suti[$j]>=0 && $suti[$j]<=255){
-                                        $ok = 1;
-                                }
+                        if(is_numeric($suti[$j])){
+                            if($suti[$j]>=0 && $suti[$j]<=255){
+                                $ok = 1;
+                            }
                         }
                 }
         }
